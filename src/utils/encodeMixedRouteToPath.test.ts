971e0aa6719cca327a03347845f3ed4252163ec5
CryptoPhantom <166310737+CryptoPhantom777@users.noreply.github.com>
2024-04-10 18:06:42 +0200

    feat: migrate to xlayerswap

--- src/utils/encodeMixedRouteToPath.test.ts
+++ src/utils/encodeMixedRouteToPath.test.ts
@@ -1,6 +1,6 @@
-import { CurrencyAmount, Ether, Token, WETH9 } from '@uniswap/sdk-core'
-import { Pair } from '@uniswap/v2-sdk'
-import { encodeSqrtRatioX96, FeeAmount, Pool } from '@uniswap/v3-sdk'
+import { CurrencyAmount, Ether, Token, WETH9 } from '@xlayerswap/sdk-core'
+import { Pair } from '@xlayerswap/v2-sdk'
+import { encodeSqrtRatioX96, FeeAmount, Pool } from '@xlayerswap/v3-sdk'
 import { MixedRouteSDK } from '../entities/mixedRoute/route'
 import { encodeMixedRouteToPath } from './encodeMixedRouteToPath'
 
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
-import { Currency, Token } from '@uniswap/sdk-core'
-import { Pair } from '@uniswap/v2-sdk'
-import { Pool } from '@uniswap/v3-sdk'
+import { Currency, Token } from '@xlayerswap/sdk-core'
+import { Pair } from '@xlayerswap/v2-sdk'
+import { Pool } from '@xlayerswap/v3-sdk'
 import { MixedRouteSDK } from '../entities/mixedRoute/route'
 
 /**
